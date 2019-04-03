5e7a905c1286e78343b888c3d0b07af1edfb38bf
Manuel Torrinha <manuel.torrinha@tecnico.ulisboa.pt>
2019-04-03 16:20:14 +0100

    Removing Ubuntu Xenial support, due to issues with systemd
    Signed-off-by: Manuel Torrinha <manuel.torrinha@tecnico.ulisboa.pt>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/) and this p
 
 This file is used to list changes made in each version of the slurm cookbook.
 
+## 1.0.1
+
+### Removed
+
+- support for Ubuntu Xenial
+
 ## 1.0.0
 
 ### Changed
--- metadata.rb
+++ metadata.rb
@@ -11,7 +11,7 @@ source_url 'https://github.com/ist-dsi/slurm-cookbook'
 description 'Installs/Configures slurm workload manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version '1.0.0'
+version '1.0.1'
 chef_version '~> 14.0'
 
 supports 'ubuntu', '> 18.0'
