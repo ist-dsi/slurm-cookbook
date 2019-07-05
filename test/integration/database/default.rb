a4f2f46fd07eb3b54a1d008abefcaf051ca11b04
Manuel Torrinha <manuel.torrinha@tecnico.ulisboa.pt>
2019-07-05 11:48:08 +0100

    updated integration tests
    Signed-off-by: Manuel Torrinha <manuel.torrinha@tecnico.ulisboa.pt>

--- test/integration/database/default.rb
+++ test/integration/database/default.rb
@@ -1,21 +1,21 @@
 if os.debian?
   if os.name == 'debian'
-    describe apt('http://mirrors.up.pt/pub/mariadb/repo/10.1/debian') do
+    describe apt('http://mirrors.up.pt/pub/mariadb/repo/10.4/debian') do
       it { should exist }
       it { should be_enabled }
     end
   elsif os.name == 'ubuntu'
-    describe apt('http://mirrors.up.pt/pub/mariadb/repo/10.1/ubuntu') do
+    describe apt('http://mirrors.up.pt/pub/mariadb/repo/10.4/ubuntu') do
       it { should exist }
       it { should be_enabled }
     end
   end
 
-  describe package('mariadb-client-10.1') do
+  describe package('mariadb-client-10.4') do
     it { should be_installed }
   end
 
-  describe package('mariadb-server-10.1') do
+  describe package('mariadb-server-10.4') do
     it { should be_installed }
   end
 
